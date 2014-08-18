075bbef69a41ef94c89ce478a40513e713444377
ZeJsters <zoher.jetpurwala@gmail.com>
2014-08-18 19:17:47 +0530

    Merkify

--- omni_i9082.mk
+++ omni_i9082.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/merk/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := GalaxyGrandDuos
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/merk/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9082/i9082.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9082
-PRODUCT_NAME := omni_i9082
+PRODUCT_NAME := merk_i9082
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-I9082
